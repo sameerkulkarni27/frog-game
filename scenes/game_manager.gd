8a7f342cf4a5b7c1e303af87de567d8ab26e004e
sameerkulkarni27 <sameerk2114@gmail.com>
2024-05-29 17:20:05 -0700

    Player score displayed with UI improvements

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -1,11 +1,15 @@
 extends Node
 
 var points = 0
+@onready var points_label = $"../UI/Panel/PointsLabel"
 
 func add_cherry_points():
 	points += 1
-	print(points)
+	#print(points)
+	points_label.text = "POINTS: " + str(points)
 
 func add_melon_points():
 	points += 2
-	print(points)
+	#print(points)
+	points_label.text = "POINTS: " + str(points)
+
