1507cdf1ed4b49c2c7c36b99444b77dabd2415cc
sameerkulkarni27 <sameerk2114@gmail.com>
2024-06-09 16:50:18 -0700

    Started particle animation

--- scenes/cherry.gd
+++ scenes/cherry.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
-		queue_free()
 		game_manager.add_cherry_points()
+		animation_player.play("collected_animation")
+		queue_free()
