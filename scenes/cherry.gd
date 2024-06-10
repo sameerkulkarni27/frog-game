04d577fcb4f1feccb1ffa1cbb4b9b6a6213aa1c1
sameerkulkarni27 <sameerk2114@gmail.com>
2024-06-10 16:50:21 -0700

    Fruit collecting animation added

--- scenes/cherry.gd
+++ scenes/cherry.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
-@onready var animation_player = $AnimationPlayer
+@onready var particle = preload("res://scenes/collected_particle.tscn")
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		game_manager.add_cherry_points()
-		animation_player.play("collected_animation")
 		queue_free()
+		
+		var collected = particle.instantiate()
+		collected.global_position = $Marker2D.global_position
+		get_parent().add_child(collected)
+		
--- scenes/melon.gd
+++ scenes/melon.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var particle = preload("res://scenes/collected_particle.tscn")
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
-		queue_free()
 		game_manager.add_melon_points()
+		queue_free()
+		
+		var collected = particle.instantiate()
+		collected.global_position = $Marker2D.global_position
+		get_parent().add_child(collected)
